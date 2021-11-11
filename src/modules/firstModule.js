5e5180a637d957d345438087496f8a0f19e1276f
vsheva <valera.sheva@gmail.com>
2021-11-11 23:10:17 +0200

    new project

---
+++ src/modules/firstModule.js
@@ -0,0 +1,6 @@
+
+const firstModule = function() {
+    console.log("I'm the first module!")
+}
+
+export default firstModule;
---
+++ src/modules/secondModule.js
@@ -0,0 +1,6 @@
+
+const secondModule = function() {
+    console.log("I'm second module1 !")
+}
+
+export default secondModule;
