920f4e0aaa8ab2b63a4f791b0858efb54f6ea112
vsheva <valera.sheva@gmail.com>
2021-11-13 01:06:57 +0200

    Timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -6,38 +6,55 @@ const timer = (deadline) => {
     const timerMinutes = document.getElementById("timer-minutes");
     const timerSeconds = document.getElementById("timer-seconds");
 
+
+
     const getTimeRemaining = () => {
         let dateStop = new Date(deadline).getTime()
         let dateNow = new Date().getTime()
         let timeRemaining = (dateStop - dateNow) / 1000
-        //let days = Math.floor(timeRemaining / 60 / 60 / 24)
-        let hours = Math.floor(timeRemaining / 60 / 60)
+        let hours = Math.floor(timeRemaining / 60 / 60)    //вычленим количество дней
         let minutes = Math.floor((timeRemaining / 60) % 60)
         let seconds = Math.floor(timeRemaining % 60);
 
-        //console.log(days)
-
         return {timeRemaining, hours, minutes, seconds }
 
-
     }
+
+
+    let getZero = function (num) {
+        if (num >=0 && num <10) {
+            return '0'+ num;
+        } else {
+            return num;
+        }
+    }
+
+
 
     const updateClock = () => {
         let getTime = getTimeRemaining()
-        timerHours.textContent = getTime.hours;
-        timerMinutes.textContent = getTime.minutes;
-        timerSeconds.textContent = getTime.seconds;
+        timerHours.textContent = getZero(getTime.hours);
+        timerMinutes.textContent = getZero(getTime.minutes);
+        timerSeconds.textContent = getZero(getTime.seconds);
 
-        if (getTime.timeRemaining >0) {
-            setTimeout(updateClock,1000)
+        if (getTime.timeRemaining <= 0) {
+            clearInterval(intervalId);
+            timerHours.textContent = "00";
+            timerMinutes.textContent = "00";
+            timerSeconds.textContent = "00";
         }
 
 
-    }
-    updateClock()
+        // if (getTime.timeRemaining < 0) {
+        //     setTimeout(updateClock,1000)
+        // }
 
-    //getTimeRemaining("20 november 2021")
-    // setInterval(countTimer, 1000)
+    }
+
+    let intervalId = setInterval(updateClock, 1000)
+
+    //updateClock()
+
 }
 
 export default timer;
