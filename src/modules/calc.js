f1c1d73ffe88195b25fb55df22b9cb3a05468969
vsheva <valera.sheva@gmail.com>
2021-11-23 20:50:47 +0200

    Добавил бегущий счетчик через setInterval

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -7,6 +7,7 @@ const calc = (price = 100) => {
     const calcDay = document.querySelector(".calc-day")
     const total = document.querySelector("#total")
 
+
     const countCalc = () => {
         const calcTypeValue = +calcType.options[calcType.selectedIndex].value
         const calcSquareValue = calcSquare.value
@@ -26,13 +27,31 @@ const calc = (price = 100) => {
             calcDayValue = 1.5
         }
 
+        // if (calcType.value && calcSquare.value) {
+        //     totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+        // } else {
+        //     totalValue = 0
+        // }
+        // total.textContent = `${totalValue} ₽`
+        //let countId,
+
         if (calcType.value && calcSquare.value) {
+
             totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
-        } else {
-            totalValue = 0
+            //Запуск анимации итога
+            let counter = 0;
+            const step = Math.round(totalValue / 100 * 25);
+            let countId = setInterval(() => {
+                if (counter >= totalValue) {
+                    clearInterval(countId);
+                    total.textContent = Math.floor(totalValue);
+                } else {
+                    counter = counter + step;
+                    total.textContent = counter;
+                }
+            }, 30);
         }
-        total.textContent = `${totalValue} ₽`
-    }
+    };
 
 
     calcBlock.addEventListener("change", (e) => {
