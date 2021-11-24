6e58706809c45f43cf217eaa4d503d3688715e18
vsheva <valera.sheva@gmail.com>
2021-11-25 01:07:11 +0200

    Helpers, animation pattern/функции помощники, паттерн анимации

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,42 +1,39 @@
+'use strict'
+import {animate} from './helpers'
+
+
 const modal = () => {
     let popUpBtn = document.querySelectorAll('.popup-btn');
-    let popup = document.querySelector('.popup');                           //popup
-    //let popUpClose = popup.querySelector('.popup-close');
+    let popup = document.querySelector('.popup');
+    let popupContent = document.querySelector('.popup-content');
 
-    let popupContent = document.querySelector('.popup-content');            //popupContent
-    let count = 0;
-    let idAnimation
-
-
-    let popupAnimation = () => {
-        idAnimation = requestAnimationFrame(popupAnimation)
-        count++;
-
-        if (count < 50) {
-            popupContent.style.top = count * 2 + "px"
-        } else {
-            cancelAnimationFrame(idAnimation)
-            count = 0;
-        }
+    const showPopup = () => {
+        animate({
+            duration: 1000,
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                // popupContent.style.top = (150 * progress) + "px"
+                popupContent.style.opacity = progress
+            }
+        });
     }
 
 
-    popUpBtn.forEach(elem => {                                            // супер!!! потренироваться писать такой код
+    popUpBtn.forEach(elem => {
         elem.addEventListener('click', () => {
             if (screen.width >= 768) {
-                idAnimation = requestAnimationFrame(popupAnimation)
+                showPopup()
             }
             popup.style.display = 'block'
         })
     });
 
-    // popUpClose.addEventListener('click', () => {
-    //     popup.style.display = 'none'
-    // })
 
     popup.addEventListener('click', (e) => {
-        if(!e.target.closest('.popup-content') || e.target.classList.contains("popup-close")) {                 // не модалка, а null ------
-            popup.style.display = 'none'                                                                        // закрытие или в поле , или в крестик модальное окно
+        if (!e.target.closest('.popup-content') || e.target.classList.contains("popup-close")) {
+            popup.style.display = 'none'
         }
     })
 }
@@ -45,3 +42,146 @@ modal()
 
 export default modal;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {animate} from './helpers'
+//
+//
+// const modal = () => {
+//     let popUpBtn = document.querySelectorAll('.popup-btn');
+//     let popup = document.querySelector('.popup');
+//     let popupContent = document.querySelector('.popup-content');
+//     let count = 0;
+//     let idAnimation
+//
+//
+//     let popupAnimation = () => {
+//         idAnimation = requestAnimationFrame(popupAnimation)
+//         count++;
+//
+//         if (count < 50) {
+//             popupContent.style.top = count * 2 + "px"
+//         } else {
+//             cancelAnimationFrame(idAnimation)
+//             count = 0;
+//         }
+//     }
+//
+//
+//     popUpBtn.forEach(elem => {
+//         elem.addEventListener('click', () => {
+//             if (screen.width >= 768) {
+//                 idAnimation = requestAnimationFrame(popupAnimation)
+//             }
+//             popup.style.display = 'block'
+//         })
+//     });
+//
+//
+//     popup.addEventListener('click', (e) => {
+//         if (!e.target.closest('.popup-content') || e.target.classList.contains("popup-close")) {
+//             popup.style.display = 'none'
+//         }
+//     })
+//
+//
+//
+// }
+//
+// modal()
+//
+// export default modal;
+//
